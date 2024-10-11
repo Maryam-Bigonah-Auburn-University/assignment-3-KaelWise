aed6e495e195dc5c79609218f4629618739a50df
KaelWise <krw0094@auburn.edu>
2024-10-11 17:58:13 -0500

    Update q1.cpp
    First change

--- assignment_3/q1.cpp
+++ assignment_3/q1.cpp
@@ -2,22 +2,51 @@
 using namespace std;
 
 int main() {
-	string first, second;
+    string first, second;
 
-	int added[20];
-    
-	int carry = 0;
-	// Get the values
-	cout << "Enter the first number:";
-	cin  >> first;
-	cout << "Enter the second number:";
-	cin  >> second;	
+    int added[21];
 
-	// Convert digits to ints
-	int x[first.length()], y[second.length()];
+    int carry = 0;
+    // Get the values
+    cout << "Enter the first number:";
+    cin  >> first;
+    cout << "Enter the second number:";
+    cin  >> second;
 
-	
-	// Add the arrays
+    // Convert digits to ints
+    int x[first.length()], y[second.length()];
 
-	return 0;
+    for (int i = first.length(); i < 1; i--) {
+
+        x[i] = first[i];
+    }
+
+    for (int j = second.length(); j < 1; j--) {
+
+        y[j] = second[j];
+    }
+
+    // Add the arrays
+
+    for (int i = 0; i < first.length(); i++) {
+
+        if (x[i] + y[i] >= 10) {
+
+            added[i] = (x[i] + y[i]) + carry;
+            carry = (x[i] + y[i]) / 10;
+        }
+
+        else {
+
+            added[i] = (x[i] + y[i]) + carry;
+            carry = 0;
+        }
+    }
+
+    for (int i = 0; i < first.length(); i++) {
+
+        cout << added[i];
+    }
+
+    return 0;
 }
