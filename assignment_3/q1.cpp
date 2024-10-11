ff75db5e626ec42477cc4fd8292c7bd2b6cf6fe3
KaelWise <krw0094@auburn.edu>
2024-10-11 18:07:11 -0500

    Update q1.cpp
    Second Change

--- assignment_3/q1.cpp
+++ assignment_3/q1.cpp
@@ -3,48 +3,47 @@ using namespace std;
 
 int main() {
     string first, second;
-
-    int added[21];
-
+    int added[21] = {0};
     int carry = 0;
+
     // Get the values
     cout << "Enter the first number:";
-    cin  >> first;
+    cin >> first;
     cout << "Enter the second number:";
-    cin  >> second;
+    cin >> second;
 
     // Convert digits to ints
     int x[first.length()], y[second.length()];
 
-    for (int i = first.length(); i < 1; i--) {
-
-        x[i] = first[i];
+    for (int i = 0; i < first.length(); i++) {
+        x[i] = first[first.length() - 1 - i] - '0';
     }
 
-    for (int j = second.length(); j < 1; j--) {
-
-        y[j] = second[j];
+    for (int j = 0; j < second.length(); j++) {
+        y[j] = second[second.length() - 1 - j] - '0';
     }
 
     // Add the arrays
+    int length = max(first.length(), second.length());
 
-    for (int i = 0; i < first.length(); i++) {
-
-        if (x[i] + y[i] >= 10) {
-
-            added[i] = (x[i] + y[i]) + carry;
-            carry = (x[i] + y[i]) / 10;
+    for (int i = 0; i < length; i++) {
+        int sum = carry;
+        if (i < first.length()) {
+            sum += x[i];
         }
-
-        else {
-
-            added[i] = (x[i] + y[i]) + carry;
-            carry = 0;
+        if (i < second.length()) {
+            sum += y[i];
         }
+        added[i] = sum % 10;
+        carry = sum / 10;
     }
 
-    for (int i = 0; i < first.length(); i++) {
+    if (carry) {
+        added[length++] = carry;
+    }
 
+    // Print the result
+    for (int i = length - 1; i >= 0; i--) {
         cout << added[i];
     }
 
