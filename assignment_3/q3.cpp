36d6168d71149a3c4f187da8e63fe6140da31239
KaelWise <krw0094@auburn.edu>
2024-10-11 22:12:27 -0500

    Update q3.cpp
    I found it easier to keep the functions separate. I am still working on incorporating the functions into the program.

--- assignment_3/q3.cpp
+++ assignment_3/q3.cpp
@@ -1,52 +1,36 @@
-#include <iostream>
-#include <iomanip>
-
+#include<iostream>
+#include<iomanip>
 using namespace std;
 
-int countOccurances(int arr[], int size, int n);
-void sortArray(int arr[], int size);
-
 int main() {
-	int size = 0;
+    //Array and size variable created
+    int size = 0, myArray[50];
+    //Input size of array
+        cout << "\nHow many numbers do you want to enter:";
+        cin >> size;
+    //Input array values
+        for(int i = 0; i < size; i++) cin >> myArray[i];
 
-	int arr[50];
-
-	// Read in the values into an array
-	cout << "Enter size of array: ";
-	cin >> size;
-
-	for(int i = 0; i < size; i++) cin >> arr[i];
-
-	sortArray(arr,size);
-
-
-
-
-	return 0;
-}
-
-int countOccurances(int *arr, int size, int n) {
-	for(int i = 0; i < n; i++) {
-		int count = 0;
-		while(arr[i] == arr[i + 1] && i < size){
-			n++;
-			i++;
-		}
-	}
-	cout << left << setw(10) << "N" << setw(10) << "Count" << endl;
-	cout << left << setw(10) << " " << arr << setw(10) << " " << size << endl;
-}
-
-// I used bubble sort since that is covered in the text for this chapter
-void sortArray(int *arr, int size) {
-	for(int i = 0; i < size; i++){
-		for(int j = 0; j <= i; j++){
-			if(arr[i] > arr[j]){
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
+    //Sort array
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j <= i; j++){
+            if(myArray[i] > myArray[j]){
+                int temp = myArray[i];
+                myArray[i] = myArray[j];
+                myArray[j] = temp;
+            }
+        }
+    }
+    //Display sorted array and unique number count
+    cout << left << setw(10) << "Number" << setw(10) << "Count" << endl;
+    for(int i = 0; i < size; i++){
+        int count = 1;
+        while(myArray[i] == myArray[i + 1] && i < size){
+            count++;
+            i++;
+        }
+        cout << left << setw(10) << myArray[i] << setw(10) << count << endl;
+    }
+    return 0;
 
 }
